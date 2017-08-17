9e5860cb3e06c3f56db35afe6c2cf39a489ac068
bodybybuddha <jtaylor@recycledpapyr.us>
2017-08-17 17:04:35 -0400

    Add ability to include a pretty display name in drop down; script contains examples

--- Scripts/SiteStatus.sh
+++ Scripts/SiteStatus.sh
@@ -2,9 +2,18 @@
 
 WORKSPACE=~/scripts/isOnline
 # list of websites. each website in new line. leave an empty line in the end.
+# each line can have an url to test and an optional displayname:
+# for example:
+# http://hostname.com
+# hostname.com/testme
+# hostname.com:8181
+# hostname.com;Pretty Display Name
 LISTFILE=$WORKSPACE/websites.lst
 # List of site/status
 SITESTATUS=$WORKSPACE/sitestatus.txt
+
+SITEUPCOUNT=0
+SITEDOWNCOUNT=0
 
 # Clean up file
 if [ -f $SITESTATUS ]; then rm -f $SITESTATUS; fi
@@ -12,7 +21,15 @@ if [ -f $SITESTATUS ]; then rm -f $SITESTATUS; fi
 # main loop
 while read p; do
 
-    response=$(curl -L --write-out %{http_code} --silent --output /dev/null $p)
+    #Load the website line into an array
+    IFS=';' read -ra WWW <<< "$p"
+    if [ ${#WWW[@]} -eq 1 ]; then
+        DISPLAYNAME=${WWW[0]}
+    else
+        DISPLAYNAME=${WWW[1]}
+    fi
+
+    response=$(curl -L --write-out %{http_code} --silent --output /dev/null ${WWW[0]})
 
     if [ $response -eq 200 ] ; then
         # Site up        
@@ -20,14 +37,14 @@ while read p; do
         SITEUPCOUNT=$[SITEUPCOUNT+1]
 
         # Add info to SITESTATUS file
-        echo -e "$p : \e[32m[ok]\e[0m" >> $SITESTATUS
+        echo -e "${DISPLAYNAME} : \e[32m[ok]\e[0m" >> $SITESTATUS
     else
         # Site Down
         # Increment counter
         SITEDOWNCOUNT=$[SITEDOWNCOUNT+1]
 
         # Add info to SITESTATUS file
-        echo -e "$p : \e[31m[DOWN]\e[0m" >> $SITESTATUS
+        echo -e "${DISPLAYNAME} : \e[31m[DOWN]\e[0m" >> $SITESTATUS
     fi
 done <$LISTFILE
 
