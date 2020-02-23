a13e94661512a24a42b40b9e8810feecd564fd41
Ian Chesal <ian.chesal@gmail.com>
2020-02-23 11:14:25 -0800

    Cover missing states and improve the interface (#67)
    * Cover two missing states and improve the interface
    
    There were two missing state cases I had failed to handle in the text >
    unicode character substitutions. Both are exceptional cases so I went
    with some warning-type characters.
    
    I also improve the UI so that you don't have to edit the script. You
    pass the NightScout URI to query as an command line argument instead.
    This should make git-pull refreshes on the repo easier for anyone using
    the script since they won't have to edit it to make it work.
    
    * Missed the NONE state
    
    Ok. Finally aligned with https://github.com/nightscout/cgm-remote-monitor/blob/41ac93f7217b1b7023ec6ad6fc35d29dcf2e4f88/lib/plugins/direction.js#L53

--- Scripts/NightScoutCGM.sh
+++ Scripts/NightScoutCGM.sh
@@ -3,14 +3,19 @@
 # The Unicode stuff is rough, but it works.
 # See: http://www.nightscout.info/
 
-# CONFIGURATION
-# Replace this URL with the URL of your NightScout setup
-NS_URL=https://cgmtest.herokuapp.com
-DATA=$(curl --silent --fail ${NS_URL}/api/v1/entries/current)
+# USE
+# Call this script with your own NightScout URL to query your setup for data
+# and print it out. Example:
+#
+#    ❯ ./NightScoutCGM.sh https://cgmtest.herokuapp.com
+#    166 → @ 09:42 AM
+
+DATA=$(curl --silent --fail ${1}/api/v1/entries/current)
 VALUE=$(echo $DATA | awk '{print $3}')
 DIRECTION=$(echo $DATA | awk '{print $4}' | sed 's/\"//g')
 TIME=$(echo $DATA | awk '{print $1}' | date +'%I:%M %p')
 # This maps the direction strings to nice Unicode characters
+DIRECTION=$(echo $DIRECTION | sed 's/NONE/⇼/')
 DIRECTION=$(echo $DIRECTION | sed 's/TripleUp/⤊/')
 DIRECTION=$(echo $DIRECTION | sed 's/DoubleUp/⇈/')
 DIRECTION=$(echo $DIRECTION | sed 's/SingleUp/↑/')
@@ -20,5 +25,7 @@ DIRECTION=$(echo $DIRECTION | sed 's/FortyFiveDown/↘/')
 DIRECTION=$(echo $DIRECTION | sed 's/SingleDown/↓/')
 DIRECTION=$(echo $DIRECTION | sed 's/DoubleDown/⇊/')
 DIRECTION=$(echo $DIRECTION | sed 's/TripleDown/⤋/')
+DIRECTION=$(echo $DIRECTION | sed 's/NOT COMPUTABLE/—/')
+DIRECTION=$(echo $DIRECTION | sed 's/RATE OUT OF RANGE/⚠︎/')
 # The end result
 echo $VALUE $DIRECTION @ $TIME
